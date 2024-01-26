72df834a383f383c0d2e99b87710fdaa1bd27020
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-01-26 23:23:06 +0700

    login button

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import {
-  StyleSheet, 
-  View, 
-  Text, 
-  Button, 
-  Linking, 
-} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 const App = () => {
-  
-}
+  return (
+    <View>
+      <View style={{marginRight: 20, marginTop: 30,}}>
+        <TouchableOpacity style={Styles.loginButton}>
+          <Text style={{fontSize: 14, fontWeight: '900',}}>Login</Text>
+
+        </TouchableOpacity>
+      </View>
+
+      <View></View>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  loginButton: {
+    padding: 10,
+    alignSelf: 'flex-end',
+    backgroundColor: '#0000000',
+  },
+});
 
 export default App;
