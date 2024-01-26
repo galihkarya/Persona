4dcb25de0d9f1023b3dbeb9860653007a87dec63
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-01-26 23:55:09 +0700

    wkwkwk

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <View style={{marginRight: 20, marginTop: 30,}}>
+      <View style={{marginRight: 10, marginTop: 20}}>
         <TouchableOpacity style={Styles.loginButton}>
-          <Text style={{fontSize: 14, fontWeight: '900',}}>Login</Text>
-
+          <Text style={Styles.loginText}>Login</Text>
+          <Image style={Styles.avatar} source={require('../assets/icons/Avatar.png')} />
         </TouchableOpacity>
       </View>
 
@@ -20,7 +20,19 @@ const Styles = StyleSheet.create({
   loginButton: {
     padding: 10,
     alignSelf: 'flex-end',
-    backgroundColor: '#0000000',
+    backgroundColor: '#00000000',
+    flexDirection: 'row',
+  },
+  loginText: {
+    marginRight: 10,
+    fontSize: 14,
+    fontWeight: '900',
+    color: '#000000',
+    alignSelf: 'center',
+  },
+  avatar: {
+    width: 40, 
+    height: 40,
   },
 });
 
