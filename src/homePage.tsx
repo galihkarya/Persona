ec5c9a6012a3b85266b2d1f41d66da3e33d3928a
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-01-30 16:19:20 +0700

    progress on home screen

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, Image, SafeAreaView} from 'react-native';
 
 const App = () => {
   return (
-    <View>
+    <SafeAreaView>
       <View style={{marginRight: 10, marginTop: 20}}>
         <TouchableOpacity style={Styles.loginButton}>
           <Text style={Styles.loginText}>Login</Text>
-          <Image style={Styles.avatar} source={require('../assets/icons/Avatar.png')} />
+          <Image
+            style={Styles.avatar}
+            source={require('../assets/icons/Avatar.png')}
+          />
         </TouchableOpacity>
       </View>
 
-      <View></View>
-    </View>
+      <View style={{marginTop: 20}}>
+        <TouchableOpacity style={Styles.card}>
+          <Image
+            style={Styles.imageObjects}
+            source={require('../assets/icons/palm_fluent.png')}
+          />
+          <Text style={Styles.textCard}>Apa itu Palmistry?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.card}>
+          <Image
+            style={Styles.imageObjects}
+            source={require('../assets/icons/palmline.png')}
+          />
+          <Text style={Styles.textCard}>Macam-macam Garis Tangan</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.card}>
+          <Image
+            style={Styles.imageObjects}
+            source={require('../assets/icons/indexfinger_fluent.png')}
+          />
+          <Text style={Styles.textCard}>Gaya belajar yang cocok sesuai kepribadian anak!</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -31,8 +56,30 @@ const Styles = StyleSheet.create({
     alignSelf: 'center',
   },
   avatar: {
-    width: 40, 
+    width: 40,
     height: 40,
+  },
+  card: {
+    flexDirection: 'row',
+    backgroundColor :'#FFFFFF',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom :10,
+    borderRadius: 12, 
+    elevation: 8,
+  },
+  imageObjects: {
+    width: 100,
+    height: 100,
+    margin: 16, 
+  }, 
+  textCard: {
+    fontSize: 18, 
+    fontWeight: '900', 
+    color: '#000000', 
+    alignSelf: 'center',
+    marginRight: 200,
   },
 });
 
